ac24d1cf27e723bc4a64e1dcb6165b83abfd4f3c
Khrystyna <khrystyna.tyvoniuk@gmail.com>
2022-11-14 02:38:12 +0100

    added footer with links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,33 @@ function App() {
      <div className='container'>
     
    <Weather defaultCity="Kyiv" />
-
+<footer>
+This project was coded by{" "}
+          <a
+            href="https://www.instagram.com/khrystyna.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Khrystyna Tyvoniuk
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KhrystynaTyvoniuk/react-practice.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://animated-alfajores-c26225.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+  
+</footer>
      </div>
     </div>
   );
